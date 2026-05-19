8340eeffa616d481e23af63b1a4a6a7e38001ef3
Elisa98-hash <puorto.elisa@hotmail.com>
2026-05-19 13:21:44 +0800

    Struttura iniziale con frontend e backend

---
+++ PersonalBackend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/send-email", async (req, res) => {
+  const { name, surname, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // SSL
+    auth: {
+      user: "elisa.puorto98@gmail.com",
+      pass: "bunc fcdl lgqt fluc",
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const mailOptions = {
+    from: '"Website" "PersonalWebsite" email',
+    to: "elisa.puorto98@gmail.com",
+    replyTo: email,
+    subject: `Nuovo messaggio da ${name} ${surname}`,
+    text: message,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, error });
+  }
+});
+
+app.listen(3000, () => console.log("Server avviato su http://localhost:3000"));
